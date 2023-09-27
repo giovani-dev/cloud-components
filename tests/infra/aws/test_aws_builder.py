f43f73695ed6085d69ba30b5a800404fd0f5c733
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-09-27 20:36:36 -0300

    feat: test aws builder

---
+++ tests/infra/aws/test_aws_builder.py
@@ -0,0 +1,36 @@
+from unittest.mock import Mock, patch
+
+from cloud_components.infra.aws.builder import AwsBuilder
+
+
+class TestAwsBuilder:
+    logger_mock: Mock
+    env_mock: Mock
+
+    def setup_method(self):
+        self.logger_mock = Mock(name="logger")
+        self.env_mock = Mock(name="env")
+
+    @patch("cloud_components.infra.aws.builder.S3")
+    @patch("cloud_components.infra.aws.builder.ResourceConnector")
+    def test_build_storage__call_s3_class__expected_call_with_mock_params(
+        self, resource_connector_mock: Mock, s3_mock: Mock
+    ):
+        methods_mock = Mock(name="resource_connector_methods")
+        methods_mock.connect.return_value = "connection"
+        resource_connector_mock.return_value = methods_mock
+
+        s3_mock.return_value = "s3 call return"
+
+        instance = AwsBuilder(logger=self.logger_mock, env=self.env_mock)
+        s3 = instance.build_storage()
+
+        resource_connector_mock.assert_called_once_with(
+            logger=self.logger_mock, env=self.env_mock
+        )
+        s3_mock.assert_called_once_with(
+            connection="connection",
+            logger=self.logger_mock,
+            env=self.env_mock,
+        )
+        assert s3 == "s3 call return"
