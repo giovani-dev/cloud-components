db287db8d660ccfced16c851f4f8cd984ed62ea0
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-11-20 15:38:45 -0300

    fix: move generics components to application layer

--- cloud_components/interface/infra/builder.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from cloud_components.interface.infra.storage import IStorage
-
-
-class IBuilder(ABC):  # pylint: disable=C0115
-    @abstractmethod
-    def build_storage(self) -> IStorage:  # pylint: disable=C0116
-        raise NotImplementedError
