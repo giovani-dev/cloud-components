a1c883e189a4b68640c1a7352e2a4c2d5efa9633
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-11-22 18:20:46 -0300

    fix: rename and move env interface

--- cloud_components/services/enviroment/factory.py
+++ cloud_components/services/enviroment/factory.py
@@ -1,10 +1,12 @@
-from cloud_components.application.interface.services.enviroment import IEnviroment
-from cloud_components.application.interface.services.log import ILog
+from cloud_components.application.interface.services.enviroment.enviroment import (
+    IEnviroment,
+)
+from cloud_components.application.interface.services.log.logger import ILogger
 from cloud_components.services.enviroment.dotenv import Dotenv
 
 
 class EnvFactory:
-    def __init__(self, logger: ILog) -> None:
+    def __init__(self, logger: ILogger) -> None:
         self.logger = logger
 
     def manufacture_dotenv(self) -> IEnviroment:
