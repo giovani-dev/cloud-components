8c14dff7a565be97c1628fab1f575091417b4ce1
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2023-11-23 19:05:45 -0300

    feat: unit test from new aws resourses

--- tests/unit/infra/aws/test_builder.py
+++ tests/unit/infra/aws/test_builder.py
@@ -11,17 +11,13 @@ class TestAwsBuilder:
         self.logger_mock = Mock(name="logger")
         self.env_mock = Mock(name="env")
 
+    @patch("cloud_components.infra.aws.builder.AwsBuilder._set_connection")
     @patch("cloud_components.infra.aws.builder.S3")
     @patch("cloud_components.infra.aws.builder.ResourceConnector")
-    def test_build_storage__call_s3_class__expected_call_with_mock_params(
-        self, resource_connector_mock: Mock, s3_mock: Mock
+    def test_build_storage_method__should_call_s3_class(
+        self, resource_connector: Mock, s3: Mock, set_connection: Mock
     ):
-        methods_mock = Mock(name="resource_connector_methods")
-        methods_mock.connect.return_value = "connection"
-        resource_connector_mock.return_value = methods_mock
-
-        s3_mock.return_value = "s3 call return"
-
+        set_connection.return_value = "fake-connection"
         instance = AwsBuilder(
             logger=self.logger_mock,
             access_key="xpto",
@@ -29,16 +25,72 @@ class TestAwsBuilder:
             env="local",
             localstack_url="http://localhost:4566",
         )
-        s3 = instance.build_storage()  # pylint: disable=C0103
+        instance.build_storage()  # pylint: disable=C0103
 
-        resource_connector_mock.assert_called_once_with(
-            self.logger_mock,
-            "xpto",
-            "abcdef",
-            "local",
-            "http://localhost:4566",
+        set_connection.assert_called_once_with(resource_name="s3")
+        s3.assert_called_once_with(
+            connection="fake-connection", logger=self.logger_mock
         )
-        s3_mock.assert_called_once_with(
-            connection="connection", logger=self.logger_mock
+
+    @patch("cloud_components.infra.aws.builder.AwsBuilder._set_connection")
+    @patch("cloud_components.infra.aws.builder.Lambda")
+    @patch("cloud_components.infra.aws.builder.ResourceConnector")
+    def test_build_function_method__should_call_lambda_class(
+        self, resource_connector: Mock, _lambda: Mock, set_connection: Mock
+    ):
+        set_connection.return_value = "fake-connection"
+        instance = AwsBuilder(
+            logger=self.logger_mock,
+            access_key="xpto",
+            secret_access_key="abcdef",
+            env="local",
+            localstack_url="http://localhost:4566",
         )
-        assert s3 == "s3 call return"
+        instance.build_function()  # pylint: disable=C0103
+
+        set_connection.assert_called_once_with(resource_name="lambda")
+        _lambda.assert_called_once_with(
+            connection="fake-connection", logger=self.logger_mock
+        )
+
+    @patch("cloud_components.infra.aws.builder.AwsBuilder._set_connection")
+    @patch("cloud_components.infra.aws.builder.Sqs")
+    @patch("cloud_components.infra.aws.builder.ResourceConnector")
+    def test_build_queue_method__should_call_sqs_class(
+        self, resource_connector: Mock, sqs: Mock, set_connection: Mock
+    ):
+        set_connection.return_value = "fake-connection"
+        instance = AwsBuilder(
+            logger=self.logger_mock,
+            access_key="xpto",
+            secret_access_key="abcdef",
+            env="local",
+            localstack_url="http://localhost:4566",
+        )
+        instance.build_queue()  # pylint: disable=C0103
+
+        set_connection.assert_called_once_with(resource_name="sqs")
+        sqs.assert_called_once_with(
+            connection="fake-connection", logger=self.logger_mock
+        )
+
+    @patch("cloud_components.infra.aws.builder.AwsBuilder._set_connection")
+    @patch("cloud_components.infra.aws.builder.Sns")
+    @patch("cloud_components.infra.aws.builder.ResourceConnector")
+    def test_build_event_method__should_call_sqs_class(
+        self, resource_connector: Mock, sns: Mock, set_connection: Mock
+    ):
+        set_connection.return_value = "fake-connection"
+        instance = AwsBuilder(
+            logger=self.logger_mock,
+            access_key="xpto",
+            secret_access_key="abcdef",
+            env="local",
+            localstack_url="http://localhost:4566",
+        )
+        instance.build_event()  # pylint: disable=C0103
+
+        set_connection.assert_called_once_with(resource_name="sns")
+        sns.assert_called_once_with(
+            connection="fake-connection", logger=self.logger_mock
+        )
