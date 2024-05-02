9871e00e352a28963a620121271b0249ea7cfb10
Giovani Liskoski Zanini <giovanilzanini@hotmail.com>
2024-05-02 18:10:29 -0300

    feat: ls and delete method in storage interface

--- cloud_components/application/interface/infra/storage.py
+++ cloud_components/application/interface/infra/storage.py
@@ -26,3 +26,11 @@ class IStorage(ABC):
     @abstractmethod
     def bucket(self, name: str):
         raise NotImplementedError
+
+    @abstractmethod
+    def ls(self, path: str) -> list[str]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete(self, file_path: str) -> bool:
+        raise NotImplementedError
